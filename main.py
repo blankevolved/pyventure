86fe31c3291881a5647634f317c79880827b0ccf
Blank <69070442+blankminecraft@users.noreply.github.com>
2023-01-08 21:58:44 -0600

    init

---
+++ main.py
@@ -0,0 +1,11 @@
+# The Steps
+#
+# 1. Design the story and game world - make a story and locations
+#  
+# 2. Create a basic structure - add game logic, I/O loop, functions for player actions, 
+# skills, UI, saving/loading ect
+#
+# 3. Add details and interactions - functions to handle specific actions, 
+# npcs, picking up items, puzzles
+#
+# 4. Testing/Debugging - clean up the game
