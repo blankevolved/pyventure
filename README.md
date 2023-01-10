6968dc4ab993a7f9df68bb9036697a4b6757576c
Blank <69070442+blankminecraft@users.noreply.github.com>
2023-01-10 00:23:20 -0600

    Commit #1

--- README.md
+++ README.md
@@ -20,19 +20,12 @@ V1.0.0 Development status: 1. Planning
 # Features
 
 1. Player actions
-
 2. Enemys
-
 3. Full skill system for player
-
 4. Easy to use player UI
-
 5. Saving/Loading from json
-
 6. NPCS/events
-
 7. Quests
-
 8. Currency system
-
 9. Inventory/Item system
+10. Bosses
--- main.py
+++ main.py
@@ -9,3 +9,85 @@
 # npcs, picking up items, puzzles
 #
 # 4. Testing/Debugging - clean up the game
+
+
+# The Story
+#
+# We fight a dragon at the end, that will be in a seprate realm/world, 
+# which will need to be unlocked,
+# with key items, level cap, certain progess obtained
+# 
+# 
+# 
+#
+
+
+# Notes
+#
+# locations are realms
+# 
+#
+
+import os
+
+def clear_screen():
+    pass
+
+class Realm(object):
+
+    def __init__(self, name:str, areas:dict={}):
+        self.name   = name
+        self.areas  = areas
+
+    def add_area(self, key:str):
+        self.areas[key] = {}
+
+    def add_area_atr(self, area:str, key:str, atr):
+        try:
+            self.areas[area][key] = atr
+        except KeyError:
+            print(f'Area "{area}" dosent exist, creating new area called "{area}".')
+            self.add_area(area)
+
+class Player(object):
+    def __init__(self, name:str):
+        self.name = name
+        self.stats = {}
+        for r in REALMS:
+            self.realms_list = {}
+            self.realms_list[r] = False
+    
+    def get_stats(self):
+        return 'Stats:'
+
+    def event_loop(self, input_object):
+        if input_object == 'stats':
+            print(self.get_stats())
+
+        if input_object == 'travel':
+            print(self.realms_list)
+
+
+REALMS = {
+    'overworld':Realm('Overworld')
+}
+
+OVERWORLD = REALMS['overworld']
+
+OVERWORLD.add_area('plains')
+
+player = Player(None)
+
+name = input('Input your name: ')
+
+player.name = name
+player.realms_list['overworld'] = True
+
+def main():
+    print(player.realms_list)
+    inp = input('>>> ')
+    player.event_loop(inp)
+
+
+while True:
+    main()
